5bfe71e1ed3882f44fe5a58fa78471de3ab988ce
snk-git-hub <shivanandu.dev@gmail.com>
2025-05-09 18:54:12 +0530

    first

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+const app=express();
+app.use(express.json())
+
+
+app.post("/api/v1/signup",(req,res)=>{
+
+})
+
+app.post("/api/v1/signin",(req,res)=>{
+    
+})
+app.post("/api/v1/content",(req,res)=>{
+    
+})
+app.post("/api/v1/getoncontent",(req,res)=>{
+    
+})
+app.post("/api/v1/deleateoncontent",(req,res)=>{
+    
+})
+app.post("/api/v1/brain/share",(req,res)=>{
+    
+})
+app.post("/api/v1/brain/:shareLink",(req,res)=>{
+    
+})
+ 
